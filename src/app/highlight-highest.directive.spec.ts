85fd65c349e0844ed48619821338ec5c9060d3c2
Ian Kabugi <iank299@gmail.com>
2019-01-17 12:10:47 +0300

    add a highlight directive to  pick the highest vpote

---
+++ src/app/highlight-highest.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HighlightHighestDirective } from './highlight-highest.directive';
+
+describe('HighlightHighestDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighlightHighestDirective();
+    expect(directive).toBeTruthy();
+  });
+});
